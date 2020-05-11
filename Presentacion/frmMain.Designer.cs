802e9ca0d8f19c7ff563f0901eb89258b5fdb3fd
JUAN MANUEL CABANILLAS LOPEZ <jcabanillasl@amseq.pe>
2020-05-11 07:31:00 -0500

    chronos v1

---
+++ Presentacion/frmMain.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Presentacion
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnDetener = new System.Windows.Forms.Button();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbltime = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timerMouse1 = new System.Windows.Forms.Timer(this.components);
+            this.timerProgram = new System.Windows.Forms.Timer(this.components);
+            this.txtProgramas = new System.Windows.Forms.TextBox();
+            this.lblReloj = new System.Windows.Forms.Label();
+            this.timerReloj = new System.Windows.Forms.Timer(this.components);
+            this.txtsololetras = new System.Windows.Forms.TextBox();
+            this.lblFlag = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnDetener
+            // 
+            this.btnDetener.Enabled = false;
+            this.btnDetener.Font = new System.Drawing.Font("Lucida Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDetener.Location = new System.Drawing.Point(245, 87);
+            this.btnDetener.Name = "btnDetener";
+            this.btnDetener.Size = new System.Drawing.Size(171, 70);
+            this.btnDetener.TabIndex = 6;
+            this.btnDetener.Text = "Detener";
+            this.btnDetener.UseVisualStyleBackColor = true;
+            this.btnDetener.Click += new System.EventHandler(this.btnDetener_Click);
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Font = new System.Drawing.Font("Lucida Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciar.Location = new System.Drawing.Point(25, 87);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(171, 70);
+            this.btnIniciar.TabIndex = 5;
+            this.btnIniciar.Text = "Inciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Sans", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(98, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(251, 32);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Control de Horas";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(485, 20);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(396, 78);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Visible = false;
+            // 
+            // lbltime
+            // 
+            this.lbltime.AutoSize = true;
+            this.lbltime.Font = new System.Drawing.Font("Lucida Sans", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltime.Location = new System.Drawing.Point(640, 296);
+            this.lbltime.Name = "lbltime";
+            this.lbltime.Size = new System.Drawing.Size(94, 32);
+            this.lbltime.TabIndex = 8;
+            this.lbltime.Text = "00:00";
+            this.lbltime.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timerMouse1
+            // 
+            this.timerMouse1.Tick += new System.EventHandler(this.timerMouse1_Tick);
+            // 
+            // timerProgram
+            // 
+            this.timerProgram.Interval = 1000;
+            this.timerProgram.Tick += new System.EventHandler(this.timerMouse2_Tick);
+            // 
+            // txtProgramas
+            // 
+            this.txtProgramas.Enabled = false;
+            this.txtProgramas.Location = new System.Drawing.Point(485, 114);
+            this.txtProgramas.Multiline = true;
+            this.txtProgramas.Name = "txtProgramas";
+            this.txtProgramas.Size = new System.Drawing.Size(406, 99);
+            this.txtProgramas.TabIndex = 9;
+            this.txtProgramas.Visible = false;
+            // 
+            // lblReloj
+            // 
+            this.lblReloj.AutoSize = true;
+            this.lblReloj.Font = new System.Drawing.Font("Lucida Sans", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblReloj.Location = new System.Drawing.Point(174, 194);
+            this.lblReloj.Name = "lblReloj";
+            this.lblReloj.Size = new System.Drawing.Size(94, 32);
+            this.lblReloj.TabIndex = 10;
+            this.lblReloj.Text = "00:00";
+            // 
+            // timerReloj
+            // 
+            this.timerReloj.Interval = 1000;
+            this.timerReloj.Tick += new System.EventHandler(this.timerReloj_Tick);
+            // 
+            // txtsololetras
+            // 
+            this.txtsololetras.Enabled = false;
+            this.txtsololetras.Location = new System.Drawing.Point(485, 220);
+            this.txtsololetras.Multiline = true;
+            this.txtsololetras.Name = "txtsololetras";
+            this.txtsololetras.Size = new System.Drawing.Size(406, 73);
+            this.txtsololetras.TabIndex = 11;
+            this.txtsololetras.Visible = false;
+            // 
+            // lblFlag
+            // 
+            this.lblFlag.AutoSize = true;
+            this.lblFlag.Location = new System.Drawing.Point(482, 315);
+            this.lblFlag.Name = "lblFlag";
+            this.lblFlag.Size = new System.Drawing.Size(37, 13);
+            this.lblFlag.TabIndex = 12;
+            this.lblFlag.Text = "lblFlag";
+            this.lblFlag.Visible = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 265);
+            this.Controls.Add(this.lblFlag);
+            this.Controls.Add(this.txtsololetras);
+            this.Controls.Add(this.lblReloj);
+            this.Controls.Add(this.txtProgramas);
+            this.Controls.Add(this.lbltime);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnDetener);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CHRONOS V2.1 (BETA)";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDetener;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbltime;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timerMouse1;
+        private System.Windows.Forms.Timer timerProgram;
+        private System.Windows.Forms.TextBox txtProgramas;
+        private System.Windows.Forms.Label lblReloj;
+        private System.Windows.Forms.Timer timerReloj;
+        private System.Windows.Forms.TextBox txtsololetras;
+        private System.Windows.Forms.Label lblFlag;
+    }
+}
---
+++ Presentacion/frmMain.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using AccesoDatos;
+using System.Data.SqlClient;
+
+namespace Presentacion
+{
+    public partial class frmMain : Form
+    {
+        public string textoEscrito = "", textoNuevo = "", posicionActual = "", posicionNueva = "", programas = "", flag = "";
+        public int numerodeveces = 0, reloj = 1, prorroga = 1;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            btnIniciar.Enabled = false;
+            btnDetener.Enabled = true;
+            timer1.Enabled = true;
+            timer1.Start();
+            timerReloj.Start();
+
+            timerMouse1.Start();
+            timerProgram.Start();
+
+            gHook.hook();
+        }
+
+        void verificarEscrito()
+        {
+            textoNuevo = textBox1.Text;
+            if (textoEscrito != textoNuevo)
+            {
+                prorroga = 1;
+                lblFlag.Text = prorroga.ToString();
+                numerodeveces = numerodeveces + 1;
+                lbltime.Text = numerodeveces.ToString();
+                textoEscrito = textoNuevo;
+            }
+            else
+            {
+                prorroga = prorroga + 1;
+                lblFlag.Text = prorroga.ToString();
+                if (prorroga < 60)
+                {
+                    numerodeveces = numerodeveces + 1;
+                    lbltime.Text = numerodeveces.ToString();
+                }
+            }
+
+        }
+
+        GlobalKeyboardHook gHook;
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            gHook = new GlobalKeyboardHook();
+            gHook.KeyDown += new KeyEventHandler(gHook_KeyDown);
+            foreach (Keys key in Enum.GetValues(typeof(Keys)))
+                gHook.HookedKeys.Add(key);
+        }
+
+        public void gHook_KeyDown(object sender, KeyEventArgs e)
+        {
+            textBox1.Text += ((char)e.KeyValue).ToString();
+            txtsololetras.Text += ((char)e.KeyValue).ToString();
+        }
+
+        void stop()
+        {
+            btnDetener.Enabled = false;
+            gHook.unhook();
+            timer1.Stop();
+            timerMouse1.Stop();
+            timerProgram.Stop();
+            timerReloj.Stop();
+            btnIniciar.Enabled = true;
+            guardar();
+            reiniciar();
+
+        }
+
+        void reiniciar()
+        {
+            textBox1.Text = "";
+            txtProgramas.Text = "";
+            txtsololetras.Text = "";
+            lblFlag.Text = "";
+            lblReloj.Text = "";
+            lbltime.Text = "";
+            numerodeveces = 0; 
+            reloj = 1;
+            prorroga = 1;
+        }
+
+        private void btnDetener_Click(object sender, EventArgs e)
+        {
+            stop();
+        }
+
+        void guardar()
+        {
+            DateTime fecha = DateTime.Now;
+
+            string nombrepc = System.Environment.MachineName;
+            string tiempoCronometrado = lblReloj.Text;
+            string tiemporeal = lbltime.Text;
+            string capturador = txtsololetras.Text;
+            capturador = capturador.Replace("'", "");
+            capturador = capturador.Replace("\"", "");
+            string programas = txtProgramas.Text;
+            programas = programas.Replace("'", "");
+            programas = programas.Replace("\"", "");
+            string fechaActual = fecha.ToString();
+
+            _0ConectDB conn = new _0ConectDB();
+            SqlDataReader dtr = null;
+            conn.cn.Open();
+            string INSERT = "INSERT INTO AMQ_ADICIONAL..HISTORIAL_TRABAJO_REMOTO ( "+
+                            "     FECHA "+
+                            "     ,NOMBRE_PC "+
+                            "     ,TIEMPO_CRONOMETRADO "+
+                            "     ,TIEMPO_REAL "+
+                            "     ,PROGRAMAS_UTILIZADOS "+
+                            "     ,EVENTOS_CAPTURADOR "+
+                            "   ) VALUES ( "+
+                            "      '"+fechaActual+"'  "+
+                            "     ,'"+nombrepc+"'  "+
+                            "     ,'"+tiempoCronometrado+"' "+
+                            "     ,'"+tiemporeal+"'   "+
+                            "     ,'"+programas+"'   "+
+                            "     ,'" + capturador + "'   " +
+                            "   )";
+            SqlCommand cmd = new SqlCommand(INSERT, conn.cn);
+            dtr = cmd.ExecuteReader();
+            dtr.Read();
+            conn.cn.Close();
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            verificarEscrito();
+
+        }
+
+
+        protected delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        protected static extern int GetWindowText(IntPtr hWnd, StringBuilder strText, int maxCount);
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        protected static extern int GetWindowTextLength(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        protected static extern bool EnumWindows(EnumWindowsProc enumProc, IntPtr lParam);
+        [DllImport("user32.dll")]
+        protected static extern bool IsWindowVisible(IntPtr hWnd);
+        public  bool EnumTheWindows(IntPtr hWnd, IntPtr lParam)
+        {
+            int tamano = GetWindowTextLength(hWnd);
+            if (tamano++ > 0 && IsWindowVisible(hWnd))
+            {
+                StringBuilder sb = new StringBuilder(tamano);
+                GetWindowText(hWnd, sb, tamano);
+                //Console.WriteLine(sb.ToString());
+                txtProgramas.Text += sb.ToString();
+            }
+            return true;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        [DllImport("user32.dll")]
+        static extern int GetForegroundWindow();
+
+        [DllImport("user32")]
+        private static extern UInt32 GetWindowThreadProcessId(Int32 hWnd, out Int32 lpdwProcessId);
+
+        private int teller = 0;
+
+
+        private void timerMouse2_Tick(object sender, EventArgs e)
+        {
+
+            //if (teller == 1)
+            //{
+                setTextje();
+            //}
+            //teller++;
+
+            //EnumWindows(new EnumWindowsProc(EnumTheWindows), IntPtr.Zero);
+            //Console.ReadKey();
+        }
+
+
+        private Int32 GetWindowProcessID(Int32 hwnd)
+        {
+            Int32 pid = 1;
+            GetWindowThreadProcessId(hwnd, out pid);
+            return pid;
+        }
+
+        private void setTextje()
+        {
+            try
+            {
+                Int32 hwnd = 0;
+                hwnd = GetForegroundWindow();
+                string appProcessName = Process.GetProcessById(GetWindowProcessID(hwnd)).ProcessName;
+                //string appExePath = Process.GetProcessById(GetWindowProcessID(hwnd)).MainModule.FileName;
+                //string appExeName = appExePath.Substring(appExePath.LastIndexOf(@"\") + 1);
+
+                if (flag == appProcessName.ToString())
+                {
+                    flag = appProcessName.ToString();
+                }
+                else
+                {
+                    //txtProgramas.Text = "";
+                    txtsololetras.Text += " | ";
+                    txtsololetras.Text += appProcessName.ToString();
+                    txtsololetras.Text += " ";
+                    flag = appProcessName.ToString();
+                }
+            }
+            catch (Exception ex)
+            { 
+            }
+
+            //txtProgramas.Text += appProcessName.ToString(); //+ " | " + appExePath + " | " + appExeName;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void timerMouse1_Tick(object sender, EventArgs e)
+        {
+            posicionNueva = Cursor.Position.X.ToString();
+            if (posicionActual != posicionNueva)
+            {
+                textBox1.Text += Cursor.Position.X.ToString();
+                posicionActual = posicionNueva;
+            }
+
+        }
+
+        private void timerReloj_Tick(object sender, EventArgs e)
+        {
+            int numero = reloj++;
+            lblReloj.Text = numero.ToString() + " (segundos)"; 
+        }
+    }
+}
